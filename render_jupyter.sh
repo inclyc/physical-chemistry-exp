00305235c2d24a197f7ac5032ae9a0d15a061e64
inclyc <axoford@icloud.com>
2022-04-12 11:19:27 +0800

    加强功能

--- render_jupyter.sh
+++ render_jupyter.sh
@@ -9,11 +9,43 @@ mkdir -p $RENDER_DIR
 
 jupyter nbconvert --to latex --output-dir=$RENDER_DIR $1
 
+function add_chinese_math_mbox() {
+    STATUS="no_math"
+    LINE_NUM=0
+    while IFS= read -r line
+    do
+        LINE_NUM=$((LINE_NUM+1))
+        if [ "$STATUS" == "no_math" ]
+        then
+            MATHSTART_EXP='\[$'
+            if [[ "$line" =~ $MATHSTART_EXP ]]
+            then
+                STATUS="math"
+            fi
+        else
+            MATH_END_EXP='\]'
+            if [ "$line" == $MATH_END_EXP ]
+            then
+                STATUS="no_math"
+            fi
+        fi
+        if [ "$STATUS" == "math" ]
+        then
+            REGEX='[^ -~]+'
+            if [[ $line =~ $REGEX ]] && [[ ! $line =~ $MATHSTART_EXP ]]
+            then
+                sed -i -E "${LINE_NUM}s/($REGEX)/"'{\\mbox{\1}}/g' $1
+            fi
+        fi
 
-
+    done < $1
+}
+add_chinese_math_mbox $RENDER_DIR$TEX_FILE_NAME
 sed -i -E 's/\\documentclass\[11pt\]\{article\}/\\documentclass[a4paper]{ctexart}/g' $RENDER_DIR/$TEX_FILE_NAME
 sed -i -E '/\\begin\{document\}/i author{'$AUTHOR'}' $RENDER_DIR/$TEX_FILE_NAME
 sed -i -E 's/author\{'$AUTHOR'\}/\\author{'$AUTHOR'}/g' $RENDER_DIR/$TEX_FILE_NAME
+
+
 
 pushd $RENDER_DIR
 latexmk -pdf -xelatex -quiet $TEX_FILE_NAME
